fb420998368544728cdbb0aa9e830ef1b7b36ae8
outskillg16 <outskillg16@gmail.com>
2026-01-08 23:32:46 -0800

    Updated WebhookService.ts

--- src/services/WebhookService.ts
+++ src/services/WebhookService.ts
@@ -1,7 +1,7 @@
 //const WEBHOOK_URL = 'https://n8n.srv1009698.hstgr.cloud/webhook/appointment-webhook';
 //const WEBHOOK_URL = 'https://nimishai2025.app.n8n.cloud/webhook-test/6f7e3c42-4d9d-44ec-abcb-8def1407e0b2';
-//const WEBHOOK_URL = 'https://nimishai2025.app.n8n.cloud/webhook-test/a3d06230-5f0b-430d-8b08-3a2069cadb79';
-const WEBHOOK_URL = 'https://nimishai2025.app.n8n.cloud/webhook/a3d06230-5f0b-430d-8b08-3a2069cadb79';
+const WEBHOOK_URL = 'https://nimishai2025.app.n8n.cloud/webhook-test/a3d06230-5f0b-430d-8b08-3a2069cadb79';
+//const WEBHOOK_URL = 'https://nimishai2025.app.n8n.cloud/webhook/a3d06230-5f0b-430d-8b08-3a2069cadb79';
 /**
  * APPOINTMENT SOURCE TRACKING FOR ANALYTICS
  *
